fe1d6bb4b16e2b1c89238ae1803be7787107c97a
Pranay Mundra <pranaymundra4510@gmail.com>
2023-04-23 18:20:03 -0400

    two_phase

--- fkc_coreset/src/main_algo.cpp
+++ fkc_coreset/src/main_algo.cpp
@@ -40,14 +40,117 @@ using namespace std;
 bool check_for_zeros(vector<int> v) {
     bool zeros = std::all_of(v.begin(), v.end(), [](int i){ return i == 0;});
     return zeros;
-    // for (auto i : v) {
-    //     if (i > 0) {
-    //         return false;
-    //     }
-    // }
-    // return true;
 }
 
+map<int, set<int>> get_posting_list(int datasetsize, string dataset_name) {
+
+    string posting_list_file = "/localdisk3/data-selection/data/metadata/cifar10/0.9/resnet-18_rf.txt";
+    std::ifstream infile(posting_list_file);
+    map<int, set<int>> posting_list;
+    if (infile.is_open()) {
+        string line;
+        int key = 0;
+        while (getline(infile, line)) {
+            stringstream ss(line);
+            string value;
+            set<int> values;
+            while (ss >> value) {
+                values.insert(stoi(value));
+            }
+
+            posting_list[key] = values;
+            key += 1;
+        }
+        
+
+    } else {
+        cout << "Error opening input file: " << posting_list_file << endl;
+    }
+    infile.close();
+
+    return posting_list;
+    
+}
+
+
+map<int, vector<int>> get_posting_list_sc(int datasetsize, string dataset_name, set<int> candidates) {
+
+    string posting_list_file = "/localdisk3/data-selection/data/metadata/cifar10/0.9/resnet-18_rf.txt";
+    std::ifstream infile(posting_list_file);
+    map<int, vector<int>> posting_list;
+    if (infile.is_open()) {
+        string line;
+        int key = 0;
+        while (getline(infile, line)) {
+            stringstream ss(line);
+            set<int>::iterator it = candidates.find(key);
+            if (it != candidates.end()) {
+                string value;
+                vector<int> values(datasetsize, 0);
+                while (ss >> value) {
+                    values[stoi(value)] += 1;
+                }
+
+                posting_list[key] = values;    
+            }
+            key += 1;
+        }
+        
+
+    } else {
+        cout << "Error opening input file: " << posting_list_file << endl;
+    }
+    infile.close();
+
+    return posting_list;
+    
+}
+
+
+set<int> set_cover(vector<int> coverage_tracker, set<int> candidates, int dataset_size, string dataset_name) {
+    set<int> solution;
+    map<int, vector<int>> posting_list = get_posting_list_sc(dataset_size, dataset_name, candidates);
+    while ((!check_for_zeros(coverage_tracker)) && solution.size() < candidates.size()) {
+        vector<int> possible_candidates;
+        set_difference(candidates.begin(), candidates.end(), solution.begin(), solution.end(), inserter(possible_candidates, possible_candidates.begin()));
+        int best_point = -1;
+        int max_score = INT_MIN;
+
+        omp_lock_t sc_write_lock;
+        omp_init_lock(&sc_write_lock);
+
+        #pragma omp parallel for
+        for (int j = 0; j < possible_candidates.size(); j++) {
+            int i = possible_candidates[j];
+            int coverage_score = inner_product(posting_list[i].begin(), posting_list[i].end(), coverage_tracker.begin(), 0);
+            omp_set_lock(&sc_write_lock);
+            if (coverage_score > max_score) {
+                best_point = i;
+                max_score = coverage_score;
+            }
+            omp_unset_lock(&sc_write_lock);
+        }
+
+        omp_destroy_lock(&sc_write_lock);
+
+        if (best_point != -1) {
+            solution.insert(best_point);
+            vector<int> pl_best = posting_list[best_point];
+            for (int i = 0; i < dataset_size; i++) {
+                if (pl_best[i] == 1) {
+                    if (coverage_tracker[i] != 0) {
+                        coverage_tracker[i] -= 1;
+                    }
+                }
+            }
+        } else {
+            cout << "Cannot find a point" << endl;
+            break;
+        }
+
+    }
+    return solution;
+}
 
 /***
  * GFKC Algorithm : computes the FKC coreset for the dataset
@@ -60,7 +163,7 @@ bool check_for_zeros(vector<int> v) {
  * @param dataset_size size of the dataset
 */
 
-void gfkc(string dataset, double coverage_factor, int distribution_req, int num_classes, int dataset_size) {
+set<int> gfkc(string dataset, double coverage_factor, int distribution_req, int num_classes, int dataset_size) {
     // Reading posting list 
     // string posting_list_file = "/localdisk3/data-selection/data/metadata/" + dataset + "/" + to_string(coverage_factor) + "/resnet-18.txt";
     // cout << posting_list_file << endl;
@@ -90,13 +193,10 @@ void gfkc(string dataset, double coverage_factor, int distribution_req, int num_
         string line;
         int key = 0;
         while (getline(infile, line)) {
-            // cout << line << endl;
-            // break;
             stringstream ss(line);
             string value;
             vector<int> values(dataset_size, 0);
             while (ss >> value) {
-                // cout << value << endl;
                 values[stoi(value)] = 1;
             }
 
@@ -137,7 +237,6 @@ void gfkc(string dataset, double coverage_factor, int distribution_req, int num_
         cout << "Error opening input file: " << labels_file << endl;
     }
     lfile.close();
-    // cout << to_string(labels_dict[50000]) << endl;
     pre_end = std::chrono::high_resolution_clock::now();
     pre_elapsed = pre_end - pre_start;
     pre_time = pre_elapsed.count();
@@ -153,7 +252,6 @@ void gfkc(string dataset, double coverage_factor, int distribution_req, int num_
     algo_start = std::chrono::high_resolution_clock::now();
     while (((!check_for_zeros(coverage_tracker)) || (!check_for_zeros(fairness_tracker))) && coreset.size() < dataset_size) {
         vector<int> possible_candidates;
-        priority_queue<pair<int, int>> scores;
         set_difference(delta.begin(), delta.end(), coreset.begin(), coreset.end(), inserter(possible_candidates, possible_candidates.begin()));
         int best_point = -1;
         int max_score = INT_MIN;
@@ -163,43 +261,20 @@ void gfkc(string dataset, double coverage_factor, int distribution_req, int num_
 
         # pragma omp parallel for
         for (int j = 0; j < possible_candidates.size(); j++) {
-            // vector<int> coverage_compute;
-            // vector<int> fairness_compute;
-            // transform(posting_list[i].begin(), posting_list[i].end(), coverage_tracker.begin(), coverage_compute.begin(), multiplies<int>());
-            // transform(labels_dict[i].begin(), labels_dict[i].end(), fairness_tracker.begin(), fairness_compute.begin(), multiplies<int>());
-            // int coverage_score = accumulate(coverage_compute.begin(), coverage_compute.end(), 0);
-            // int fairness_score = accumulate(fairness_compute.begin(), fairness_compute.end(), 0);
             int i = possible_candidates[j];
             int coverage_score = inner_product(posting_list[i].begin(), posting_list[i].end(), coverage_tracker.begin(), 0);
             int fairness_score = inner_product(labels_dict[i].begin(), labels_dict[i].end(), fairness_tracker.begin(), 0);
-            // cout << "Coverage score is " << coverage_score << endl;
-            // cout << "Fairness score is " << fairness_score << endl;
             omp_set_lock(&write_lock);
             if ((coverage_score + fairness_score) > max_score) {
                 best_point = i;
                 max_score = coverage_score + fairness_score;
             }
-            
-            // scores.push(make_pair(coverage_score + fairness_score, i));
             omp_unset_lock(&write_lock);
         }
         omp_destroy_lock(&write_lock);
 
-        // pair<int, int> possible_point = scores.top();
-        // if (possible_point.first > max_score) {
-        //     best_point = possible_point.second;
-        // }
-
         if (best_point != -1) {
-            // cout << "Found a point" << endl;
             coreset.insert(best_point);
-            // cout << "Coreset size: " << to_string(coreset.size()) << endl;
-            // vector<int> temp_coverage_tracker;
-            // vector<int> temp_fairness_tracker;
-            // transform(posting_list[best_point].begin(), posting_list[best_point].end(), coverage_tracker.begin(), temp_coverage_tracker.begin(), minus<int>());
-            // transform(labels_dict[best_point].begin(), labels_dict[best_point].end(), fairness_tracker.begin(), temp_fairness_tracker.begin(), minus<int>());
-            // coverage_tracker = temp_coverage_tracker;
-            // fairness_tracker = temp_fairness_tracker;
             vector<int> pl_best = posting_list[best_point];
             for (int i = 0; i < dataset_size; i++) {
                 if (pl_best[i] == 1) {
@@ -218,26 +293,10 @@ void gfkc(string dataset, double coverage_factor, int distribution_req, int num_
                 }
             }
 
-            // int satisfied_points = 0;
-            // for (auto i : coverage_tracker) {
-            //     if (i == 0) {
-            //         satisfied_points += 1;
-            //     }
-            // }
-
-            // cout << "Number of satisfied points: " << satisfied_points << endl;
-
         } else {
             cout << "Cannot find a point" << endl;
             break;
         }
-
-        // if (check_for_zeros(fairness_tracker)) {
-        //     cout << "Satisfied" << endl;
-        // } else {
-        //     cout << "not satisfied" << endl;
-        // }
-        
     }
     algo_end = std::chrono::high_resolution_clock::now();
     algo_elapsed = algo_end - algo_start;
@@ -245,18 +304,23 @@ void gfkc(string dataset, double coverage_factor, int distribution_req, int num_
 
     cout << "Algorithm time: " << to_string(algo_time) << endl;
     
+    return coreset;
 }
+
+
+
+
 
 /**
  * ManyToMany Swapping Algorithm 
  * @param coverage_coreset coreset that satisfies K-coverage
- * @param posting_list for each point, the points it represents
  * @param dataset_name name of the dataset
  * @param num_classes number of classes/groups in the dataset
  * @param dataset_size size of the dataset
  * @param distribution_req fairness constraint for groups
+ * @param coverage_factor number of representatives for each point in the coreset
 */
-void many_to_many_swap(set<int> coverage_coreset, map<int, vector<int>> posting_list, string dataset_name, int num_classes, int dataset_size, int distribution_req) {
+void many_to_many_swap(set<int> coverage_coreset, string dataset_name, int num_classes, int dataset_size, int distribution_req, int coverage_factor) {
     map<int, set<int>> labels_to_points;
     map<int, set<int>> coverage_coreset_distribution;
     set<int> g_left;
@@ -264,6 +328,8 @@ void many_to_many_swap(set<int> coverage_coreset, map<int, vector<int>> posting_
     set<int> L;
     set<int> R;
     set<int> delta_minus_coverage_coreset;
+    vector<int> coverage_tracker(dataset_size, 0);
+    map<int, set<int>> posting_list = get_posting_list(dataset_size, dataset_name);
 
     for (int d = 0; d < dataset_size; d++) {
         set<int>::iterator it = coverage_coreset.find(d);
@@ -321,14 +387,50 @@ void many_to_many_swap(set<int> coverage_coreset, map<int, vector<int>> posting_
         }
     }
 
-    for (auto gl : g_left) {
-        set_intersection(delta_minus_coverage_coreset.begin(), delta_minus_coverage_coreset.end(), labels_to_points[gl].begin(), labels_to_points[gl].end(), inserter(R, R.begin()));
+    /**
+     * @todo what if they're aren't any points you can swap based on the number of groups in g_extra. In this case we just random sample for each group that was left
+    */
+
+    if (L.size() > 0) {
+        for (auto gl : g_left) {
+            set_intersection(delta_minus_coverage_coreset.begin(), delta_minus_coverage_coreset.end(), labels_to_points[gl].begin(), labels_to_points[gl].end(), inserter(R, R.begin()));
+        }
+
+        for (auto l : L) {
+            coverage_coreset.erase(l);
+            // calculate affected points
+            for (auto x : posting_list[l]) {
+                set<int> representatives;
+                set_intersection(coverage_coreset.begin(), coverage_coreset.end(), posting_list[x].begin(), posting_list[x].end(), inserter(representatives, representatives.begin()));
+                if (representatives.size() < coverage_factor) {
+                    for (auto r : representatives) {
+                        coverage_tracker[r] += 1;
+                    }
+                }
+                representatives.clear();
+            }
+        }
+
+        set<int> r_star = set_cover(coverage_tracker, R, dataset_size, dataset_name);
+
+        for (auto r : r_star) {
+            coverage_coreset.insert(r);
+        }
+
+
+        
+    } else {
+        // we can't swap so we add randomly 
+        for (auto gl : g_left) {
+            int number_of_points = distribution_req - coverage_coreset_distribution[gl].size();
+            set<int> R_temp;
+            set_intersection(delta_minus_coverage_coreset.begin(), delta_minus_coverage_coreset.end(), labels_to_points[gl].begin(), labels_to_points[gl].end(), inserter(R_temp, R_temp.begin()));
+            std::sample(R_temp.begin(), R_temp.end(), inserter(coverage_coreset, coverage_coreset.begin()), number_of_points, std::mt19937 {std::random_device{} ()});
+        }
     }
 
-
-
-
-
+    cout << "Solution Size: " << coverage_coreset.size() << endl;
+    
 }
 
 
@@ -354,6 +456,7 @@ int main(int argc, char const *argv[]) {
     int dataset_size = 50000;
     // cout << coverage_factor << endl;
 
-    gfkc(dataset, coverage_factor, distribution_req, num_classes, dataset_size);
+    set<int> coverage_coreset = gfkc(dataset, coverage_factor, 0, num_classes, dataset_size);
+    many_to_many_swap(coverage_coreset, dataset, num_classes, dataset_size, distribution_req, coverage_factor);
     return 0;
 }
